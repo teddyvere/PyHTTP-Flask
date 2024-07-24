a5912d761033f8e5cc45389fb669e43b8f0ed62a
Teddy Archibald <107772813+teddyvere@users.noreply.github.com>
2024-07-24 18:44:39 -0400

    fin

--- app.py
+++ app.py
@@ -1,7 +1,19 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route('/')
-def zipcode_data():
-    return 'Hello, ZipCode!'
+def index():
+    return render_template('index.html')
+
+@app.route('/form', methods=['GET', 'POST'])
+def form():
+    if request.method == 'POST':
+        name = request.form['name']
+        email = request.form['email']
+        return f'Name: {name}, Email: {email}'
+    # else: the request method is GET
+    return render_template('form.html')
+
+if __name__ == '__main__':
+    app.run(port=8000)
