87a2885ce836136cc98931f1bedda9728a2dcca2
Teddy Archibald <107772813+teddyvere@users.noreply.github.com>
2024-07-24 18:14:24 -0400

    one

---
+++ name_email.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/form', methods=['GET', 'POST'])
+def form():
+    if request.method == 'POST':
+        name = request.form['name']
+        email = request.form['email']
+        return f'Name: {name}, Email: {email}'
+    # else: the request method is GET
+    return render_template('form.html')
